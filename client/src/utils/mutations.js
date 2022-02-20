2fc86167638f349bf1ff53770ff7696c57ba359f
Jason Offenbaker <JasonOffenbaker@gmail.com>
2022-02-19 18:05:13 -0700

    added queries and mutations outlines

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,25 @@
+import {gql} from "@apollo/client"
+
+export const LOGIN__USER = gql`
+    mutation loginUser(){
+    }
+
+`
+
+export const ADD__USER = gql`
+    mutation loginUser(){
+    }
+    
+`
+
+export const SAVE__BOOK = gql`
+    mutation loginUser(){
+    }
+    
+`
+
+export const REMOVE__BOOK = gql`
+    mutation loginUser(){
+    }
+    
+`
